6eeacd735f0f51e125dcaa15e958a10cd957035b
sgich <73955932+sgich@users.noreply.github.com>
2021-04-24 18:12:24 +0200

    update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import transformers
-import sys
-sys.path.append("./src")
+#import sys
+#sys.path.append("./src")
 
 BERT_PATH = "bert-base-uncased"
-MODEL_PATH = "./model.bin"
+MODEL_PATH = "src/model.bin"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
 
 #tokens added to vocab for this particular case
