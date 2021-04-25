f469965d71b2015ce94a21964858fd4424fd55a2
sgich <73955932+sgich@users.noreply.github.com>
2021-04-25 12:39:49 +0200

    update config

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import transformers
+#import sys
+#sys.path.append("./src")
 
-BERT_PATH = "C:/Users/sgich/Desktop/EaglesFinal/input/bert_base_uncased/"
-MODEL_PATH = "C:/Users/sgich/Desktop/EaglesFinal/src/model.bin"
+BERT_PATH = "bert-base-uncased"
+MODEL_PATH = "src/model.bin"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
+
+#tokens added to vocab for this particular case
 TOKENIZER.add_tokens(["😃","😁","😆","😡","🤬", "🖕","💯","💩","🤢","🤮"])
