0b85053cf59eb8b7619120f143a23a213949c491
sgich <73955932+sgich@users.noreply.github.com>
2021-04-25 21:26:23 +0200

    Update hate_speech_model.py

--- hate_speech_model.py
+++ hate_speech_model.py
@@ -1,11 +1,9 @@
 import config
 import transformers
 import torch.nn as nn
-import streamlit as st
 
 
 class HateSpeechClassifier(nn.Module):
-    @st.cache(allow_output_mutation=True)
     def __init__(self):
         super(HateSpeechClassifier, self).__init__()
         self.bert = transformers.BertModel.from_pretrained(config.BERT_PATH, return_dict=False)
@@ -13,7 +11,6 @@ class HateSpeechClassifier(nn.Module):
         self.drop = nn.Dropout(p=0.3)
         self.out = nn.Linear(768, 1)
 
-    @st.cache(allow_output_mutation=True)
     def forward(self, input_ids, attention_mask):
         _, pooled_output = self.bert(
             input_ids=input_ids,
